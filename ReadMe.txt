1b83c2a809a88b603999df80cd4f0f92aa4e60a4
Syuichi Murashima <mura-github@vwnet.jp>
2019-11-02 11:22:51 +0900

    1st commit

---
+++ ReadMe.txt
@@ -0,0 +1,31 @@
+﻿Jpeg 画像方向を正位置にする
+
+■ これは何?
+ Jpeg 画像方向を正位置にします
+
+■ 使い方
+PowerShell プロンプトで以下コマンド入力すると、Jpeg の画像方向を正位置にします
+オリジナルファイルは -ORG.jpg として残ります
+
+SetNormalPositionExif ファイルPath
+
+■ セットの仕方
+PowerShell プロンプトで install.ps1 を実行してください
+
+■ Uninstall 方法
+uninstall.ps1 を実行して下さい
+
+■ 動作確認環境
+On Windows
+	5.1
+	6.2.1
+	7.0.0-preview.5
+
+■ Web サイト
+Jpeg ファイルの GPS Exif を PowerShell で削除する
+https://www.vwnet.jp/Windows/PowerShell/2016051501/RemoveGPSExif.htm
+
+■ リポジトリ
+https://github.com/MuraAtVwnet/Remove-GPS-Exif
+git@github.com:MuraAtVwnet/Remove-GPS-Exif.git
+
---
+++ SetNormalPositionExif.psm1
@@ -0,0 +1,57 @@
+﻿#######################################################
+# Jpeg 画像方向を正位置にする
+#######################################################
+function SetNormalPositionExif( $JpegFile ){
+
+	# 拡張子チェック
+	$FileName = Split-Path $JpegFile -Leaf
+	if( (($FileName -split "\.")[1] -ne "jpg" ) -and (($FileName -split "\.")[1] -ne "jpeg" )){
+		return
+	}
+
+	# フルパスにする
+	$JpegFileFullName = Convert-Path $JpegFile -ErrorAction SilentlyContinue
+	if( $JpegFileFullName -eq $null ){
+		echo "$JpegFile not convert full path."
+		return
+	}
+
+	# 存在確認
+	if( -not ( Test-Path $JpegFileFullName )){
+		echo "$JpegFile not found"
+		return
+	}
+
+	# テンポラリファイル名
+	$TempFile = $JpegFileFullName + ".tmp"
+
+	# アセンブリロード
+	Add-Type -AssemblyName System.Drawing
+
+	# Jpeg 読み込み
+	$bmp = New-Object System.Drawing.Bitmap($JpegFileFullName)
+
+	# Exif を処理
+	$Index = $bmp.PropertyItems.Length
+	for ( $i = 0; $i -lt $Index; $i++ ){
+		$Item = $bmp.PropertyItems[$i]
+		$ID = $Item.Id
+
+		# 回線方向を正位置にする
+		if( $id -eq 274 ){
+			$Item.Value[0] = 1
+			$bmp.SetPropertyItem($Item)
+		}
+	}
+
+	# ファイル出力
+	$bmp.Save($TempFile, [System.Drawing.Imaging.ImageFormat]::Jpeg )
+	$bmp.Dispose()
+
+	# オリジナルとテンポラリファイルを入れ替える
+	$Org_Path = Split-Path -Path $JpegFileFullName
+	$Org_FileName = Split-Path -Leaf $JpegFileFullName
+	$New_FileName = $Org_FileName.Split(".")[0] + "-ORG." + $Org_FileName.Split(".")[1]
+	ren $JpegFileFullName $New_FileName
+	ren $TempFile $JpegFileFullName
+}
